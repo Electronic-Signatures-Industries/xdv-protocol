9ccb05599f54e9ec84c538d90608a07c07a38a2f
Rogelio <molekilla@gmail.com>
2020-10-29 20:12:07 -0500

    init commit

---
+++ xdv_core/example/xdv_core_example.dart
@@ -0,0 +1,6 @@
+import 'package:xdv_core/xdv_core.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ xdv_core/test/xdv_core_test.dart
@@ -0,0 +1,16 @@
+import 'package:xdv_core/xdv_core.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
